1f1ea77d59c6abe22eeef48a64c274426e4ce79c
Tolkyn <tolken.sak@gmail.com>
2026-08-01 02:46:21 +0500

    Fix AnimatedTransition Param

--- app/components/AnimatedTransition.tsx
+++ app/components/AnimatedTransition.tsx
@@ -4,7 +4,7 @@
 
 import { motion } from 'framer-motion';
 
-export default function AnimatedTransition({ children }) {
+export default function AnimatedTransition({ children }: { children: React.ReactNode }) {
     return (
         <motion.div
             initial={{ opacity: 0, y: 20 }}
